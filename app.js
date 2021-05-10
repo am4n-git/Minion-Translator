633e94bb834321e989fec7296aa48d8265de93b5
am4n-raj <am4nraj@gmail.com>
2021-05-10 15:31:41 +0530

    feat updated
    Added Buttons- sample text, clear
    Updated style

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var btn= document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#text-input");
 var output = document.querySelector("#output");
+var samplebtn = document.querySelector("#sampletext");
+var clearbtn= document.querySelector("#clear");
+
 
 var url = "https://api.funtranslations.com/translate/minion.json"
 
@@ -8,20 +11,47 @@ function getTranslation(text){
     return url + "?" + "text="+text;
 }
 
-function errorHandler(){
-    alert("Something wrong with error, try later");
+function errorHandler(error){
+    alert("Too Many Requests: Rate limit of 5 requests per hour exceeded.\n Try again later!!");
 }
 
 function clickHandler(){
-    var inputText = txtInput.value;
 
-    fetch(getTranslation(inputText))
-        .then(response => response.json())
-        .then(json => {
+    var inputText = txtInput.value
+
+    if(inputText==""){
+        alert("Enter your message in the box");
+    }
+
+    else{
+        fetch(getTranslation(inputText))
+            .then(response => response.json())
+            .then(json => {
             var translatedText= json.contents.translated;
             output.innerText= translatedText;
         })
-        .catch(errorHandler)
+            .catch(errorHandler)
+    }
+
 };
 
+
+function sampleEvent()
+{
+    txtInput.innerText="Hello this is a sample text";
+    btn.addEventListener("click", clickHandler);
+
+
+}
+
+function clear(){
+    txtInput.value= "";
+    output.innerText= "";
+}
+
+
+
 btn.addEventListener("click", clickHandler);
+samplebtn.addEventListener("click", sampleEvent);
+clearbtn.addEventListener("click", clear);
+
